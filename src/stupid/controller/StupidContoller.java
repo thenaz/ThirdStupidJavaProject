17c150cb872278b9511762a0dcb5d49492d66cc8
thenaz <anaz7963@740s-pl-126.local>
2016-09-06 09:55:48 -0600

    Added things to ThirdStupid

---
+++ src/stupid/controller/StupidContoller.java
@@ -0,0 +1,8 @@
+package stupid.controller;
+
+public class StupidContoller {
+public static void main (String [] args){
+}
+	StupidController firstAppController = new StupidController () ;
+	firstAppController .start();
+}
