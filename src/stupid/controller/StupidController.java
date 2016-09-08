40788e6c096f52b54b07939bfa64794e640bb7f9
thenaz <anaz7963@740s-pl-126.local>
2016-09-08 09:25:37 -0600

    Made little convo
    9/8

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,16 +1,29 @@
 package stupid.controller;
 
-public class StupidController {
+import java.util.Scanner;
+
+public class StupidController
+{
 	private String myName;
-	
+	private Scanner inputReader;
+
 	public StupidController()
 	{
 		myName = "Ammar Nazir";
+		inputReader = new Scanner(System.in);
 	}
 
 	public void start()
 	{
 		System.out.println("this program is less stupid");
 		System.out.println(myName);
+		askQuestions();
+	}
+	
+	private void askQuestions()
+	{
+		System.out.println("How was your day?");
+		String input = inputReader.nextLine();
+		System.out.println("Oh, you said: " + input + " about your day. Nice bro ");
 	}
 }
