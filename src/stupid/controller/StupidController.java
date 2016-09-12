f5ad12ad22a234faeb73bc35f191f1bc756ab502
thenaz <anaz7963@740s-pl-126.local>
2016-09-12 08:36:27 -0600

    Added many questions to ThirdStupidJavaProject
    9/12

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -25,5 +25,25 @@ public class StupidController
 		System.out.println("How was your day?");
 		String input = inputReader.nextLine();
 		System.out.println("Oh, you said: " + input + " about your day. Nice bro ");
+		
+		System.out.println("My next question is this: What is the best type of food?");
+		String foodInput = inputReader.next();
+		System.out.println("OK, you typed this: " + foodInput);
+		
+		System.out.println("Another question: What is one of your hobbies?");
+		String hobbyInput = inputReader.next();
+		System.out.println("Oh, you like: " + hobbyInput);
+		
+		System.out.println("I have to ask: What is your favorite class in school?");
+		String schoolInput = inputReader.next();
+		System.out.println("Whoa, I also like " + schoolInput);
+		
+		System.out.println("Just curious: When is your birthday?");
+		String birthdayInput = inputReader.next();
+		System.out.println("Nice. Mine is close to " + birthdayInput);
+		
+		
+		
 	}
+	
 }
